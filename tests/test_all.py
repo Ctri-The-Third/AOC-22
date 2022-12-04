848911b0a0bd3b1249aafadfd974a2ea20c41827
C'tri Goudie <CtriCoding@Ctri.co.uk>
2022-12-04 11:34:49 +0000

    Update test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -9,14 +9,14 @@ from models.day_0 import problem
 
 def test_day_1():
     from models.day_1 import Day_1
-    d1 = Day_1(r".\problems\day1_sample.txt")
+    d1 = Day_1(r"problems\day1_sample.txt")
     d1:problem 
     assert d1.solve() == "24000,45000"
 
 
 def test_day_2():
     from models.day_2 import Day_2
-    day = Day_2(r".\problems\day2_sample.txt")
+    day = Day_2(r"problems\day2_sample.txt")
     day:problem 
     assert day.solve() == "15,12"
 
